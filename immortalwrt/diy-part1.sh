36c7733faf7824cac11c6dd041f9e1e8de8cb452
wzyqlxp <58631828+wzyqlxp@users.noreply.github.com>
2025-12-06 13:34:11 +0800

    Update diy-part1.sh

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -17,15 +17,18 @@ rm -rf feeds/luci/applications/luci-app-argon-config
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
-git clone --depth=1 https://github.com/fw876/helloworld
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
+# 移除ssr-plus/helloworld（翻墙相关）
+# git clone --depth=1 https://github.com/fw876/helloworld
+# 移除passwall2
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
 git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki
 git clone --depth=1 https://github.com/DHDAXCW/dhdaxcw-app
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 git clone --depth=1 https://github.com/linkease/istore
 git clone --depth=1 https://github.com/Siriling/5G-Modem-Support && rm -rf 5G-Modem-Support/rooter
-git clone --depth=1 https://github.com/gdy666/luci-app-lucky
+# 移除Lucky
+# git clone --depth=1 https://github.com/gdy666/luci-app-lucky
 popd
 
 # add luci-app-mosdns
